6ea3418806b7e3be6c04ad1f2b85b5b591ac1815
Kirill Galimov <rufixrus@gmail.com>
2020-06-12 18:09:29 +0200

    Initial commit

---
+++ getters.js
@@ -0,0 +1,83 @@
+function selfRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomSex() {
+  const index = selfRandom(0, 1);
+  return sexList[index];
+}
+
+function getRandomAge() {
+  let randomAge = selfRandom(18, 90);
+  if (randomAge > 60) {
+    randomAge = randomAge - 15;
+  }
+  return randomAge;
+}
+
+function getChildFree() {
+  const index = selfRandom(0, 10);
+  if (index > 2) {
+    return childFree[1];
+  } else {
+    return childFree[0];
+  }
+}
+
+function getRandomJob() {
+  const index = selfRandom(0, jobList.length - 1);
+  const randomJob = jobList[index];
+  jobList.splice(index, 1);
+  return randomJob;
+}
+
+function getRandomHealth() {
+  const index = selfRandom(0, healthList.length - 1);
+  const randomHealth = healthList[index];
+  healthList.splice(index, 1);
+  return randomHealth;
+}
+
+function getRandomCharacter() {
+  const index = selfRandom(0, characterList.length - 1);
+  const randomCharacter = characterList[index];
+  characterList.splice(index, 1);
+  return randomCharacter;
+}
+
+function getRandomHobby() {
+  const index = selfRandom(0, hobbyList.length - 1);
+  const randomHobby = hobbyList[index];
+  hobbyList.splice(index, 1);
+  return randomHobby;
+}
+
+function getRandomPhobia() {
+  const index = selfRandom(0, phobiaList.length - 1);
+  const randomPhobia = phobiaList[index];
+  phobiaList.splice(index, 1);
+  return randomPhobia;
+}
+
+function getRandomBaggage() {
+  const index = selfRandom(0, baggageList.length - 1);
+  const randomBaggage = baggageList[index];
+  baggageList.splice(index, 1);
+  return randomBaggage;
+}
+
+function getRandomAdditional() {
+  const index = selfRandom(0, additionalList.length - 1);
+  const randomAdditional = additionalList[index];
+  additionalList.splice(index, 1);
+  return randomAdditional;
+}
+
+function getRandomAction() {
+  const index = selfRandom(0, actionList.length - 1);
+  const randomAction = actionList[index];
+  actionList.splice(index, 1);
+  return randomAction;
+}
